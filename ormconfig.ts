beb46e12a00325a3172f8b8c0fd0b8a475c08143
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2022-07-31 10:54:33 -0300

    feat: adds .env and .env example

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,14 +1,23 @@
+import 'dotenv/config';
 import { DataSource } from 'typeorm';
 import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+
+const {
+  MYSQL_HOST,
+  MYSQL_PORT,
+  MYSQL_USERNAME,
+  MYSQL_PASSWORD,
+  MYSQL_DATABASE,
+} = process.env;
 
 // para app module
 const config: MysqlConnectionOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'Ivan123.',
-  database: 'library',
+  host: MYSQL_HOST,
+  port: +MYSQL_PORT,
+  username: MYSQL_USERNAME,
+  password: MYSQL_PASSWORD,
+  database: MYSQL_DATABASE,
   // entities: [],
   // entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   // entities: [join(__dirname, '**', '*.entity.{ts,js}')],
--- src/modules/book/book.dto.ts
+++ src/modules/book/book.dto.ts
@@ -1,9 +1,11 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateBookDto {
   @IsString()
+  @IsNotEmpty()
   title: string;
 
   @IsNumber()
+  @IsNotEmpty()
   author_id: number;
 }
