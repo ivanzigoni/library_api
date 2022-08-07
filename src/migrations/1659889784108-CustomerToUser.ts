b646044d302977c1efb1c4ae5c5dfac061c17f28
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2022-08-07 13:40:53 -0300

    feat: migration

---
+++ src/migrations/1659889784108-CustomerToUser.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CustomerToUser1659889784108 implements MigrationInterface {
+  name = 'CustomerToUser1659889784108';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+
+    ALTER TABLE customer
+    RENAME user,
+    ADD role varchar(255); 
+
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+
+    ALTER TABLE user
+    RENAME customer,
+    DROP COLUMN role;
+
+    `);
+  }
+}
--- src/modules/customer/interfaces/customer.entity.ts
+++ src/modules/customer/interfaces/customer.entity.ts
@@ -1,7 +1,7 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class Customer {
+export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
